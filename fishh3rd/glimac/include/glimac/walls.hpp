385e08385c939e13f86981346229c7503aa81334
DANLCARTON <ericthiberge@orange.fr>
2023-06-03 10:25:45 +0200

    walls.cpp cleaned

--- fishh3rd/glimac/include/glimac/walls.hpp
+++ fishh3rd/glimac/include/glimac/walls.hpp
@@ -8,10 +8,5 @@
 #include "common.hpp"
 
 namespace glimac {
-
-    void avoidWall(Fish &wallFish, Fish &fish, double WALLS_RADIUS, double WALLS_STRENGTH, double TURN_FACTOR);
-    void wall(Fish &fish, double distance, const int wall, double AREA, double WALLS_RADIUS, double WALLS_STRENGTH, double TURN_FACTOR);
-    void wallSeparation(Fish &fish, double AREA, double WALLS_RADIUS, double WALLS_STRENGTH, double TURN_FACTOR);
     void passTrough(Fish &fish, double AREA);
-
 }
